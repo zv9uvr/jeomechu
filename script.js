0a19a16913699f0ac2d94baa2dc29766366ca5c9
zv9uvr <n4yeon99@gmail.com>
2024-11-02 01:33:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+window.onload = function() {
+    // 지도의 중심좌표와 초기화 설정
+    const container = document.getElementById('map'); // 'map' id 확인
+    const options = {
+        center: new kakao.maps.LatLng(37.5665, 126.9780), // 서울 좌표 예시
+        level: 3
+    };
+
+    const map = new kakao.maps.Map(container, options);
+};
+
+
 function showRestaurantScreen(category) {
     currentCategory = category;
     document.getElementById('menuScreen').classList.add('hidden');
